1b50a5b1ee3b4f63643a0ce7c4ad72e362b1131f
EwenBALOUIN <46872095+EwenBALOUIN@users.noreply.github.com>
2020-02-10 16:36:39 +0100

    :wrench: env files

---
+++ src/environments/environment.common.ts
@@ -0,0 +1,6 @@
+export const commonEnvironment = {
+    nasa: {
+        baseUrl: 'https://api.nasa.gov/mars-photos/api/v1/rovers/',
+        apiKey: 'Xir2W2pGbpExcRdf95vtHb99iE6BQgpm7XA33mnx',
+    }
+};
