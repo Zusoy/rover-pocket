1fbd535d58016b7319b12f033529e1dd6d4808a7
EwenBALOUIN <46872095+EwenBALOUIN@users.noreply.github.com>
2020-03-09 14:39:20 +0100

    :sparkles: add api service nasa

---
+++ src/app/services/nasaApi.ts
@@ -0,0 +1,54 @@
+import {Injectable} from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+/**
+* NASA API Service
+* 
+* @since    0.0.1
+* @category Service
+* @author   Zusoy
+*/
+@Injectable({
+    providedIn: 'root'
+})
+export class NasaAPI
+{
+
+    /**
+    * Send request to NASA API
+    * @param url
+    * @param method
+    */
+    public async request(url: string, method: string = 'GET')
+    {
+        let apiURL = this.makeNasaApiURL(url);
+        let options = {
+            method: method,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+
+        let result = await fetch(apiURL, options).then(response => {
+            let json = response.json()
+            return json;
+        });
+
+        return result;
+    }
+
+
+    /**
+    * Make NASA API URL
+    * @param url
+    */
+    public makeNasaApiURL(url: string): string
+    {
+        let apiUrl = environment.nasa.baseUrl + url;
+        let querySeparator = (apiUrl.indexOf('?') != -1)
+        ? '&'
+        : '?';
+
+        return (apiUrl + querySeparator + `api_key=${environment.nasa.apiKey}`);
+    }
+}
